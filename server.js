ac17670bba415d8e612c3c3fb3b84c18a205eb9c
cristian-nambo <cristian-nambo@hotmail.com>
2023-06-03 13:33:16 -0700

    create and new routes complete

--- server.js
+++ server.js
@@ -26,9 +26,11 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
 // 404 Page
 app.get('*', (req, res) => {
   res.send('404 Page not found')
 })
+
 
