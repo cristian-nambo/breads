2d0e2878532be2f4695d3f184b793b62eb1e7212
cristian-nambo <cristian-nambo@hotmail.com>
2023-06-13 23:16:23 -0700

    Added custom Helper Methods

--- views/show.jsx
+++ views/show.jsx
@@ -6,6 +6,7 @@ function Show ({bread, index}) {
     // console.log(bread.name)
       return (
         <Default>
+          
             <h3>{bread.name}</h3>
             <p>
                 and it
@@ -17,13 +18,17 @@ function Show ({bread, index}) {
                 have gluten.
           </p>
           <img src={bread.image} alt={bread.name} />
-          <li>
-            <a href="/breads">Go home</a>
-          </li>         
-          <form action={`/breads/${index}?_method=DELETE`} method="POST">
+          <p>{bread.getBakedBy()}</p>
+          <form>
+            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
+          </form>          
+          <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
             <input type='submit' value="DELETE"/>
           </form>
-          <form><a href={`/breads/${index}/edit`}><button>Edit</button></a></form>      
+          <li>
+            <a href="/breads">Go home</a>
+          </li>
+     
         </Default>
 
       )
